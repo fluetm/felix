4fcc79f674447df807dfe130be4fc123e3af8172
Matt Fluet <matt.fluet@appian.com>
2019-12-09 14:36:02 -0500

    Classloader Ordering: Prefer Bundle Classes Option

--- src/main/java/org/apache/felix/framework/BundleWiringImpl.java
+++ src/main/java/org/apache/felix/framework/BundleWiringImpl.java
@@ -93,6 +93,9 @@ public class BundleWiringImpl implements BundleWiring
         }
     };
 
+    public final static String PREFER_BUNDLE_CLASSES_PROPERTY = "felix.framework.preferbundleclasses";
+    private final boolean m_preferBundleClasses = Boolean.parseBoolean(System.getProperty(PREFER_BUNDLE_CLASSES_PROPERTY, "false"));
+
     private final Logger m_logger;
     private final Map m_configMap;
     private final StatefulResolver m_resolver;
@@ -1584,10 +1587,13 @@ public class BundleWiringImpl implements BundleWiring
                             }
                         }
 
-                        // Look in the revision's imports. Note that the search may
-                        // be aborted if this method throws an exception, otherwise
-                        // it continues if a null is returned.
-                        result = searchImports(pkgName, name, isClass);
+                        // Search available imports first, unless directed to prefer classes bundled inside the plugin.
+                        if (!m_preferBundleClasses) {
+                            // Look in the revision's imports. Note that the search may
+                            // be aborted if this method throws an exception, otherwise
+                            // it continues if a null is returned.
+                            result = searchImports(pkgName, name, isClass);
+                        }
 
                         // If not found, try the revision's own class path.
                         if (result == null)
@@ -1609,6 +1615,11 @@ public class BundleWiringImpl implements BundleWiring
                             else
                             {
                                 result = m_revision.getResourceLocal(name);
+                            }
+
+                            // If the imports haven't been searched yet and we haven't found the resource, try them now.
+                            if (m_preferBundleClasses && result == null) {
+                                result = searchImports(pkgName, name, isClass);
                             }
 
                             // If still not found, then try the revision's dynamic imports.
